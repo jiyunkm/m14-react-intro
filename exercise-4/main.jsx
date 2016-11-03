fc9236d7f340038389838899b113ad7ad1166b0d
jiyunkm <jiyunkm@uw.edu>
2016-11-03 10:21:17 -0700

    react

--- exercise-4/main.jsx
+++ exercise-4/main.jsx
@@ -51,15 +51,18 @@ var EmployeeSearch = React.createClass({
         return({searchString:''});
     },
     // Add a filter funciton
-
+    filter:function(event) {
+      var value = event.target.value;
+      this.setState({searchString:value})
+    },
     render:function() {
-        var employees = this.props.data;
-
+        var employees = this.props.data.filter(d) => d.name.match(this.state.searchString));
         // Use this.state.searchString to filter down the `employees` array
+
 
         return(
             <div>
-                <input placeholder="Search employees"/>
+                <input onChange={this.filter} placeholder="Search employees"/>
                 <EmployeeTable data={employees}/>
             </div>
         )
